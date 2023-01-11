25889654320afc0ff44f1527ffb0b4f670f3bc9f
Jason <jasonanggah@gmail.com>
2023-01-11 14:31:05 +0700

    add rating

--- src/features/restaurants/componenets/restaurant-info-card.components.js
+++ src/features/restaurants/componenets/restaurant-info-card.components.js
@@ -1,6 +1,39 @@
 import React from "react";
-import { StyleSheet, Text } from "react-native";
+import { Text, View } from "react-native";
 import { Card } from "react-native-paper";
+import styled from "styled-components/native";
+import { SvgXml } from "react-native-svg";
+import stars from "../../../../assets/stars";
+
+const Title = styled(Text)`
+  background-color: ${(props) => props.theme.colors.bg.primary};
+  font-family: ${(props) => props.theme.fonts.heading};
+  font-size: ${(props) => props.theme.fontSizes.body};
+`;
+
+const CardStyle = styled(Card)`
+  background-color: ${(props) => props.theme.colors.bg.primary};
+`;
+
+const CardCover = styled(Card.Cover)`
+  background-color: ${(props) => props.theme.colors.bg.primary};
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+const Info = styled(View)`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+const Address = styled(Text)`
+  font-family: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSizes.caption};
+`;
+
+const Rating = styled.View`
+  flex-direction: row;
+  padding-top: ${(props) => props.theme.space[2]};
+  padding-bottom: ${(props) => props.theme.space[2]};
+`;
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
@@ -9,22 +42,26 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
     photos = [
       "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
     ],
-    // address = "100 some random street",
+    address = "100 some random street",
     // isOpenNow = true,
-    // rating = 4,
+    rating = 4,
     // isClosedTemporarily,
   } = restaurant;
 
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
+
   return (
-    <Card elevation={5} style={styles.card}>
-      <Card.Cover key={name} style={styles.cover} source={{ uri: photos[0] }} />
-      <Text style={styles.title}>{name}</Text>
-    </Card>
+    <CardStyle elevation={5}>
+      <CardCover key={name} source={{ uri: photos[0] }} />
+      <Info>
+        <Title>{name}</Title>
+        <Rating>
+          {ratingArray.map(() => (
+            <SvgXml xml={stars} width={20} height={20} />
+          ))}
+        </Rating>
+        <Address>{address}</Address>
+      </Info>
+    </CardStyle>
   );
 };
-
-const styles = StyleSheet.create({
-  card: { backgroundColor: "white" },
-  cover: { padding: 20, backgroundColor: "white" },
-  title: { padding: 16 },
-});
--- src/features/restaurants/screens/restaurant.screen.js
+++ src/features/restaurants/screens/restaurant.screen.js
@@ -1,30 +1,31 @@
 import React from "react";
-import { StyleSheet, View, StatusBar, SafeAreaView } from "react-native";
+import { View, StatusBar, SafeAreaView } from "react-native";
 import { Searchbar } from "react-native-paper";
+import styled from "styled-components/native";
 import { RestaurantInfoCard } from "../componenets/restaurant-info-card.components";
 
-export const RestaurantsScreen = () => (
-  <SafeAreaView style={styles.container}>
-    <View style={styles.search}>
-      <Searchbar />
-    </View>
-    <View style={styles.list}>
-      <RestaurantInfoCard />
-    </View>
-  </SafeAreaView>
-);
+const SafeArea = styled(SafeAreaView)`
+  flex: 1;
+  margintop: ${StatusBar.currentHeight}px;
+`;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight,
-  },
-  search: {
-    padding: 16,
-  },
-  list: {
-    flex: 1,
-    padding: 16,
-    backgroundColor: "blue",
-  },
-});
+const SearchContainer = styled(View)`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+const RestaurantListContainer = styled(View)`
+  flex: 1;
+  padding: ${(props) => props.theme.space[3]};
+  background-color: ${(props) => props.theme.colors.bg.success};
+`;
+
+export const RestaurantsScreen = () => (
+  <SafeArea>
+    <SearchContainer>
+      <Searchbar />
+    </SearchContainer>
+    <RestaurantListContainer>
+      <RestaurantInfoCard />
+    </RestaurantListContainer>
+  </SafeArea>
+);
